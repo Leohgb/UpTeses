15ba4058a2287f6a4f0acc05677bad6973f61887
Leonardo  Leal <91156801+Leohgb@users.noreply.github.com>
2023-01-03 19:09:08 -0300

    Alterando botão de confirmação de email

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -24,12 +24,15 @@ module.exports.sendConfirmationEmail = (name, email, confirmacao) => {
       subject: "Por favor confirme sua conta",
       html: `<h1>Confirmação de Email</h1>
           <h2>Olá ${name}</h2>
+          
           <p>Obrigado por criar uma conta. Por favor confirme seu Email clicando no link abaixo</p>
-          <form action="http://localhost:8081/usuarios/registro/${confirmacao}" method="POST">
-          <input type='submit' style="margin-top:5px; cursor: pointer; border-color:#17a2b8; border: 1px solid transparent; background-color:#17a2b8; color:#fff; border-radius: 0.25rem; font-size: 1rem;
+          <a href="http://localhost:8081/usuarios/registro/${confirmacao}" type='submit' style="margin-top:5px; cursor: pointer;
+          border-color:#17a2b8;  text-decoration: none;
+          border: 1px solid transparent; background-color:#17a2b8; 
+          color:#fff; border-radius: 0.25rem; font-size: 1rem;
           transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-          padding: 0.375rem 0.75rem;"
-          value='Confirme' />
+          padding: 0.375rem 0.75rem;"> Confirme  </a>
+          <br>
           </form>
           </div>`,
     }).catch(err => console.log(err));
